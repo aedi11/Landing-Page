24f7ebc868f4292a8f8f2124c5dab911580f1d5e
BuildTools <ee1221782@iitd.ac.in>
2026-02-23 09:37:32 +0530

    added analytics

--- frontend3/src/app/layout.tsx
+++ frontend3/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter, Space_Grotesk } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next"
 import "./globals.css";
 
 const inter = Inter({
@@ -40,6 +41,7 @@ export default function RootLayout({
         className={`${inter.variable} ${spaceGrotesk.variable} antialiased`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
